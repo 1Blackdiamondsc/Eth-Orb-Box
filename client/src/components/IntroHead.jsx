5942795ed3e5b94059c6fce6a4c49b6384e8b0a9
Archie Smyth <archiesmyth26@googlemail.com>
2020-12-16 02:08:27 +0000

    Hotfix for IntroHead image.

--- client/src/components/IntroHead.jsx
+++ client/src/components/IntroHead.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import Image from 'material-ui-image';
 import Box from '@material-ui/core/Box';
-import asset from './ComponentAssets/head.png'
+import head from './ComponentAssets/head.png'
 
 export default function IntroHead() {
     return (
         <Box boxShadow={4} mt={3} mb={3}>
-            <Image src={asset} aspectRatio={5/1}/>
+            <Image src={head} aspectRatio={5/1}/>
         </Box>
 
     )
