7c44560825b44dd4038d5f50922e4accf725c863
Archie Smyth <archiesmyth26@googlemail.com>
2020-12-16 03:31:34 +0000

    yy

--- client/src/components/IntroHead.jsx
+++ client/src/components/IntroHead.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import Image from 'material-ui-image';
 import Box from '@material-ui/core/Box';
-import head from './ComponentAssets/head.png'
+import img from './ComponentAssets/head.png'
 
 export default function IntroHead() {
     return (
         <Box boxShadow={4} mt={3} mb={3}>
-            <Image src={head} aspectRatio={5/1}/>
+            <Image src={img} aspectRatio={5/1}/>
         </Box>
 
     )
