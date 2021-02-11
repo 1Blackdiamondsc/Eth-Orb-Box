48820cc40ae4d6867ed5e8e3daedae0d1a712a3a
Archie Smyth <archiesmyth26@googlemail.com>
2021-02-11 17:33:03 +0000

    Matic/Polygon deployment test

--- truffle-config.js
+++ truffle-config.js
@@ -15,6 +15,13 @@ module.exports = {
       },
       network_id: '3',
     },
+    matic: {
+      provider: () => new HDWalletProvider(mnemonic, `https://rpc-mumbai.matic.today`),
+      network_id: 80001,
+      confirmations: 2,
+      timeoutBlocks: 200,
+      skipDryRun: true
+    },
     develop: {
       host: "127.0.0.1",
       port: 8545
