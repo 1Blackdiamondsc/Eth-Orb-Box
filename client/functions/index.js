5913b9feb01a3f2fd7e269d9dd32b94e0eb594d9
Archie Smyth <archiesmyth26@googlemail.com>
2021-02-16 21:14:07 +0000

    functions tweaks

--- client/functions/index.js
+++ client/functions/index.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 const Web3 = require('web3');
 const Contract = require('web3-eth-contract');
 
+
 const corsOptions = {
   origin: 'https://test-cf-97bfc.web.app',
   optionsSuccessStatus: 200 ,
@@ -11,8 +12,11 @@ const corsOptions = {
 
 
 //define express app use the use property and pass in cors options. res.setHeader( "Access-Control-Allow-Origin" ,"https://test-cf-97bfc.web.app" )
-const app = express();
 
+const app = express();
+app.use(cors());
+
+app.use(express.json()) 
 /*
 app.use(cors({
   origin: function(origin, callback){   
@@ -24,50 +28,48 @@ app.use(cors({
   }
 }));
 */
-app.get('/', (req, res) => {
+
+app.post('/', cors(corsOptions), async (req, res) => {
+   //grab the account address of the player (from front-end)
+   const  account  = req.body.account;
+   console.log('account' + account);
+   //changing to matic node
+   const web3 = new Web3.providers.HttpProvider('https://ropsten.infura.io/v3/83029d3d4a454afa9e7d1a7fdd484c13');
+   console.log(web3)
+   const artifact = axios.get('https://firebasestorage.googleapis.com/v0/b/test-cf-97bfc.appspot.com/o/abis%2Fabi.json?alt=media&token=26bb0e2f-872c-4ee9-ac0f-525b9d84af39');
+   const address = "0xE981aFEeA8E3dB77003C1D56e7c1D42e470Ea775";
+   Contract.setProvider(web3);
+   const contract = new Contract(artifact, address);
+ 
+   const supplyReq = new Promise(async (resolve, reject) => {
+     try {
+       const supply = await contract.totalSupply().call()
+       resolve(supply)
+     } catch (error) {
+       reject(error)
+     }
+   });
+   
+   const newSupply = await supplyReq + 1;
+   console.log('newsupply' + newSupply)
+   const itemtoMint = `https://test-cf-97bfc-default-rtdb.firebaseio.com/NFTs/NFT${newSupply}`
+   console.log('item to mint' + itemtoMint)
+   //contract address needed for instantiation 
+   await contract.methods.awardItem(account, itemtoMint).send({ from: account }).on('receipt', function () {
+     
+   }).then(res.status(200).send([{ txResolved: "NFT awarded post", account: account }]))
+     .catch(res.status(400).end)
+ 
+ 
+
+});
+app.get('/', cors(corsOptions), (req, res) => {
 
   res.send([
     { one: "easy", two: "hard" },
   ]);
 });
 
-//
-app.post('/', async (req, res) => {
-
-  res.setHeader( "Access-Control-Allow-Origin" ,"*" )
-   //grab the account address of the player (from front-end)
- 
-  const { account } = req.account
-  console.log('account' + account);
-  //changing to matic node
-  const web3 = new Web3.providers.HttpProvider('https://ropsten.infura.io/v3/83029d3d4a454afa9e7d1a7fdd484c13');
-  console.log(web3)
-  const artifact = axios.get('https://firebasestorage.googleapis.com/v0/b/test-cf-97bfc.appspot.com/o/abis%2Fabi.json?alt=media&token=26bb0e2f-872c-4ee9-ac0f-525b9d84af39');
-  const address = "0xE981aFEeA8E3dB77003C1D56e7c1D42e470Ea775";
-  Contract.setProvider(web3);
-  const contract = new Contract(artifact, address);
-
-  const supplyReq = new Promise(async (resolve, reject) => {
-    try {
-      const supply = await contract.totalSupply().call()
-      resolve(supply)
-    } catch (error) {
-      reject(error)
-    }
-  });
-  
-  const newSupply = await supplyReq + 1;
-  console.log('newsupply' + newSupply)
-  const itemtoMint = `https://test-cf-97bfc-default-rtdb.firebaseio.com/NFTs/NFT${newSupply}`
-  console.log('item to mint' + itemtoMint)
-  //contract address needed for instantiation 
-  await contract.methods.awardItem(account, itemtoMint).send({ from: account }).on('receipt', function () {
-    
-  }).then(res.status(200).send([{ txResolved: "NFT awarded post", account: account }]))
-    .catch(res.status(400).end)
-
-
-})
 
 
 exports.testAPI = functions.https.onRequest(app);
